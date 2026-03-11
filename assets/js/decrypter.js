3af723e7b8f207645839781785611f2262e034c0
dedetive <ded31459@hotmail.com>
2026-03-11 18:05:08 -0300

    Add hash change listener to decrypter

--- assets/js/decrypter.js
+++ assets/js/decrypter.js
@@ -4,21 +4,27 @@ let sanitizedPostContent = postContent.trim();
 sanitizedPostContent = sanitizedPostContent.replace("<p>", "")
 sanitizedPostContent = sanitizedPostContent.replace("</p>", "")
 
-const hash = window.location.hash.substring(1);
-const params = new URLSearchParams(hash);
-const secretKey = params.get('key');
+function decryptPost() {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const secretKey = params.get('key');
 
-if (!secretKey) {
-    document.getElementById('post-content').innerHTML = "This post is encrypted!<br><br>Try asking dedetive if you think this is for you. She might give you the key.";
-} else {
-    try {
-        const decrypted = CryptoJS.AES.decrypt(sanitizedPostContent, secretKey).toString(CryptoJS.enc.Utf8);
-        if (decrypted) {
-            document.getElementById('post-content').innerHTML = decrypted;
-        } else {
+    if (!secretKey) {
+        document.getElementById('post-content').innerHTML = "This post is encrypted!<br><br>Try asking dedetive if you think this is for you. She might give you the key.";
+    } else {
+        try {
+            const decrypted = CryptoJS.AES.decrypt(sanitizedPostContent, secretKey).toString(CryptoJS.enc.Utf8);
+            if (decrypted) {
+                document.getElementById('post-content').innerHTML = decrypted;
+            } else {
+                document.getElementById('post-content').innerHTML = "Wrong key, sorry!";
+            }
+        } catch (e) {
             document.getElementById('post-content').innerHTML = "Wrong key, sorry!";
         }
-    } catch (e) {
-        document.getElementById('post-content').innerHTML = "Wrong key, sorry!";
     }
 }
+
+window.addEventListener('hashchange', decryptPost);
+
+decryptPost();
